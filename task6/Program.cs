34ca89aa285761b493d40f6cc7098c2882611712
Dmitriy <dmitriy.shehobalov@gmail.com>
2022-09-20 12:48:44 +0300

    Add task6

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдает, является ли число четным
+
+Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = num % 2;
+if (res == 0)
+{
+    Console.Write($"Even number of {num} ");
+}
+else
+{
+    Console.Write($"Not even number of {num} ");
+}
